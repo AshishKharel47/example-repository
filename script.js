8370e8cce7d43892df16e73f79b7bcbf491afcb3
AshishKharel <ashishkharel58@gmail.com>
2021-03-25 20:48:52 -0400

    Changes

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var Name = prompt("Please enter your name", "<name goes here>");
+
+if (Name!= null) {
+
+    document.getElementById("Here") =
+
+    "Hello " + Name + "! How are you today?";
+
+}
